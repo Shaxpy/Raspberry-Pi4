21ddad2aaf83b964d26087fb316ab1a727cabaad
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2021-03-03 15:14:58 +0530

    Create install.sh

---
+++ Intel D455/Opencv/install.sh
@@ -0,0 +1,6 @@
+# Run this as >>> ./install.sh
+sudo apt-get update & 
+sudo apt-get upgrade &
+sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev &
+sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev &
+sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
